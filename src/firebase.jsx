5ebb91ee484308c81c4ef8e287f613eb853e5504
Peter Choi <ptrchoi@gmail.com>
2020-04-10 01:21:19 -0500

    Updating Firebase config to align with backend consolidation of Firebase projects

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -2,13 +2,14 @@ import firebase from 'firebase';
 
 // App's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'AIzaSyCXRPOX6oTDg6XvX3yUw5i2jpIb7KAzRLs',
-	authDomain: 'pomodoro-clock-ad6c9.firebaseapp.com',
-	databaseURL: 'https://pomodoro-clock-ad6c9.firebaseio.com',
-	projectId: 'pomodoro-clock-ad6c9',
-	storageBucket: 'pomodoro-clock-ad6c9.appspot.com',
-	messagingSenderId: '526738038698',
-	appId: '1:526738038698:web:6f0c951bcd50e300cc15d3'
+	apiKey: '***REMOVED***',
+	authDomain: '***REMOVED***',
+	databaseURL: '***REMOVED***',
+	projectId: 'ptrchoi-portfolio',
+	storageBucket: '***REMOVED***',
+	messagingSenderId: '***REMOVED***',
+	appId: '1:***REMOVED***:web:***REMOVED***',
+	measurementId: '***REMOVED***'
 };
 
 // Initialize Firebase
